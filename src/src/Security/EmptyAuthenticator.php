2536436e28a579bd3f05f7bc8f7a1bb74f3ac9ea
amin.bentani@hetic.net <amin.bentani@hetic.net>
2022-10-19 20:12:21 +0200

    add bundle security, and fix the bug serveur buffe
    added bundle security, and fix the bug serveur buffers size , added  features login , logout, csrf for form security

--- src/src/Security/EmptyAuthenticator.php
+++ src/src/Security/EmptyAuthenticator.php
@@ -2,51 +2,69 @@
 
 namespace App\Security;
 
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
+use Symfony\Component\Security\Http\Util\TargetPathTrait;
 
-class EmptyAuthenticator extends AbstractAuthenticator
-{
-    public function supports(Request $request): ?bool
-    {
-        // TODO: Implement supports() method.
-      return  $request->attributes->get('_route') ==='app_login'
-          && $request->isMethod('POST');
+class EmptyAuthenticator extends AbstractAuthenticator {
+  use TargetPathTrait;
+  private UrlGeneratorInterface $urlGenerator;
+
+  public function __construct(UrlGeneratorInterface $urlGenerator)
+  {
+    $this->urlGenerator = $urlGenerator;
+  }
+
+
+  public function supports( Request $request )
+  : ?bool {
+    // TODO: Implement supports() method.
+    return $request->attributes->get( '_route' ) === 'app_login'
+           && $request->isMethod( 'POST' );
+  }
+
+  public function authenticate( Request $request )
+  : Passport {
+    // TODO: Implement authenticate() method.
+
+
+    $email    = $request->request->get( 'email' );
+    $password = $request->request->get( 'password' );
+    $csrf = $request->request->get('csrf');
+
+    return new Passport(
+        new UserBadge( $email ),
+        new PasswordCredentials( $password ),
+        [
+          new CsrfTokenBadge('login_form', $csrf)
+        ]
+    );
+  }
+
+  public function onAuthenticationSuccess( Request $request, TokenInterface $token, string $firewallName )
+  : ?Response {
+    // TODO: Implement onAuthenticationSuccess() method.
+    if ( $targetPath = $this->getTargetPath( $request->getSession(), $firewallName ) ) {
+      return new RedirectResponse( $targetPath );
     }
 
-    public function authenticate(Request $request): Passport
-    {
-        // TODO: Implement authenticate() method.
+    return new RedirectResponse( $this->urlGenerator->generate( 'app_login' ) );
+  }
 
-
-      $email = $request->request->get('email');
-      $password = $request->request->get('password');
-
-      return new Passport(
-          new UserBadge($email),
-          new PasswordCredentials($password)
-      );
-    }
-
-    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
-    {
-        // TODO: Implement onAuthenticationSuccess() method.
-
-      return RedirectResponse('/login');
-
-    }
-
-    public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
-    {
-        // TODO: Implement onAuthenticationFailure() method.
-      dd($request);
-    }
+  public function onAuthenticationFailure( Request $request, AuthenticationException $exception )
+  : ?Response {
+    // TODO: Implement onAuthenticationFailure() method.
+    dd( $request );
+  }
 
 //    public function start(Request $request, AuthenticationException $authException = null): Response
 //    {
