2fc264279fa4ffca71f835b0e126c2f7d6bb610d
amin.bentani@hetic.net <amin.bentani@hetic.net>
2022-10-26 22:08:13 +0200

    add fix bug

--- src/src/DataFixtures/AppFixtures.php
+++ src/src/DataFixtures/AppFixtures.php
@@ -16,12 +16,24 @@ class AppFixtures extends Fixture {
   public function load( ObjectManager $manager )
   : void {
 
-    TagFactory::createMany( 6 );
+
+
+    TagFactory::findOrCreate( [ "name" =>"Immobilier" ]);
+    TagFactory::findOrCreate( [ "name" =>"Véhicules" ]);
+    TagFactory::findOrCreate( [ "name" =>"Emploi" ]);
+    TagFactory::findOrCreate( [ "name" =>'Maison' ]);
+    TagFactory::findOrCreate( [ "name" =>"Services" ]);
+    TagFactory::findOrCreate( [ "name" =>'Loisirs' ]);
+    TagFactory::findOrCreate( [ "name" =>'Mode' ]);
+    TagFactory::findOrCreate( [ "name" =>'Animaux' ]);
+    TagFactory::findOrCreate( [ "name" =>'Divers' ]);
+
     UserFactory::createMany( 40 );
     ArticleFactory::createMany( 120, function () {
       return [
           'idUser' => UserFactory::random(),
           'idTag'  => TagFactory::random(),
+          'slug' => 'title'
       ];
     } );
     QuestionFactory::createMany( 20, function () {
