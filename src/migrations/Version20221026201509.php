056e354e8a7a662feab3f9705de344d359ee14d5
lorde78 <62546853+lorde78@users.noreply.github.com>
2022-10-26 22:40:12 +0200

    push

--- src/migrations/Version20221026201509.php
+++ src/migrations/Version20221026201509.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20221026200528 extends AbstractMigration
+final class Version20221026201509 extends AbstractMigration
 {
     public function getDescription(): string
     {
