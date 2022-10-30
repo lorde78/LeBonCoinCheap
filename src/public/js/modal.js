f4318898232f4109108d99f9bf6733408c125214
lorde78 <62546853+lorde78@users.noreply.github.com>
2022-10-30 13:03:48 +0100

    added

--- src/public/js/modal.js
+++ src/public/js/modal.js
@@ -1,10 +1,25 @@
-document.addEventListener("DOMContentLoaded", () => {
-    document.querySelector(".modal-button").addEventListener("click", function() {
-      document.querySelector(".modal-container").classList.add("fade-in");
-    })
-    document.querySelectorAll(".modal-exit").forEach((elem) => {
-      elem.addEventListener("click", () => {
-        document.querySelector(".modal-container").classList.remove("fade-in");
-      })
-    })
-  })
+// document.addEventListener("DOMContentLoaded", () => {
+//     document.querySelector(".modal-button").addEventListener("click", function() {
+//       document.querySelector(".modal-container").classList.add("fade-in");
+//     })
+//     document.querySelectorAll(".modal-exit").forEach((elem) => {
+//       elem.addEventListener("click", () => {
+//         document.querySelector(".modal-container").classList.remove("fade-in");
+//       })
+//     })
+//   })
+
+const mainImage = document.getElementById("main-image");
+const images = document.querySelectorAll(".product__image");
+
+images.forEach((image) => {
+	image.addEventListener("click", (event) => {
+		mainImage.src = event.target.src;
+
+		document
+			.querySelector(".product__image--active")
+			.classList.remove("product__image--active");
+
+		event.target.classList.add("product__image--active");
+	});
+});
